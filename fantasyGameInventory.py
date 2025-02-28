b2ae36bec883109d116f4d8e8d0f8b21ff67d28e
Techkicker33 <51103495+Techkicker33@users.noreply.github.com>
2025-02-27 21:22:12 -0500

    updated inventory search

--- fantasyGameInventory.py
+++ fantasyGameInventory.py
@@ -65,11 +65,11 @@ def displayInventory(inventory):
     print("Inventory:")
     item_total = 0
     for k, v in inventory.items():
-        # FILL THIS PART IN
+        item_total += inventory.values([k,v])
         print("Total number of items: " + str(item_total))
 
 displayInventory(stuff)
-
+"""
 def addToInventory(inventory, addedItems):
     # your code goes here
     for k,v in inventory().items:
@@ -83,4 +83,4 @@ def addToInventory(inventory, addedItems):
 inv = {'gold coin': 42, 'rope': 1}
 dragonLoot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
 inv = addToInventory(inv, dragonLoot)
-displayInventory(inv)
+displayInventory(inv)"""
