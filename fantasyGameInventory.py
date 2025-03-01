2d2c05eab073ccd50e9434d31ff0e6097e0abd0c
Techkicker33 <51103495+Techkicker33@users.noreply.github.com>
2025-02-28 20:03:43 -0500

    Update fantasyGameInventory.py

--- fantasyGameInventory.py
+++ fantasyGameInventory.py
@@ -61,7 +61,7 @@ Total number of items: 48'''
 
 stuff = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
 
-def displayInventory(item):
+
 def displayInventory(item):
     print("Inventory:")
     print(item)
